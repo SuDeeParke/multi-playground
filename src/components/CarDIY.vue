9b04859c4f087c8fd103b6f20a8e42a161768295
sudongpeng <sudongpeng@uino.com>
2022-10-27 17:04:46 +0800

    ✨ feat: 代码与样式多项细节调整

--- src/components/CarDIY.vue
+++ src/components/CarDIY.vue
@@ -2,54 +2,79 @@
   <div class="UI">
     <ul>
       <li>
-        <button>车身</button>
-        <div class="colors">
-          <span v-for="(item, index) in COLORs" :style="{ 'background-color': item }" @click="changeColor(item)"></span>
-        </div>
+        <button><img class="carbody" :src="carbodyImg">车身</button>
+      </li>
+      <li>
+        <button><img :src="wheelsImg">轮毂</button>
+      </li>
+      <li>
+        <button><img :src="brakeImg">刹车</button>
       </li>
     </ul>
+    <div class="colors">
+      <span v-for="item in COLORs" :style="{ 'background-color': item }" @click="changeColor(item)"></span>
+    </div>
   </div>
+  
   <div class="car-diy" ref="container"></div>
 </template>
 
 <script setup lang="ts">
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment';
+
+import brakeImg from '@/assets/images/brake.png' 
+import carbodyImg from '@/assets/images/carbody2.png' 
+import wheelsImg from '@/assets/images/wheels.png' 
+
 
 const CAR = ref();
 const BODY_COLOR = ref<THREE.Mesh>();
 const WHEEL_COLOR = ref<THREE.Mesh>();
 const BRACK_COLOR = ref<THREE.Mesh>();
-
 const COLORs = ['#eff5f9', '#000000', '#252625', '#5e131d', '#8b7a82','#00194b']
 
+const container = ref()
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xaaaaaa)
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.x = 40
-camera.position.y = 40
-camera.position.z = 25
-camera.lookAt(new THREE.Vector3(0,0,0))
-
 const renderer = new THREE.WebGLRenderer({
   antialias: true
 })
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
 const controls = new OrbitControls(camera, renderer.domElement)
 
 
+const setting = () => {
+  // camera
+  // TODO 设置平移参数，并禁止之后平移
+  camera.position.x = 20
+  camera.position.y = 10
+  camera.position.z = 40
+  camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+  //renderer
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  const pmremGenerator = new THREE.PMREMGenerator(renderer)
+  pmremGenerator.compileEquirectangularShader()
+
+  // environment
+  scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture
+  scene.background = new THREE.Color(0x333333)
+  
+}
+
+
+
 const addLight = () => {
-  const light = new THREE.AmbientLight(0xf3f3f3, 0.2); // soft white light
+  const light = new THREE.SpotLight(0xffffff, 0.89, 190,(Math.PI * 45) / 180, 0.5, 0.2);
+  light.position.set(50,50,50)
+  light.castShadow = true;           
   scene.add(light);
-  const pinteLight = new THREE.PointLight(0xffffff, 1, 1000);
-  pinteLight.position.set(0, 50, 0);
- 
-  pinteLight.castShadow = true;
-  scene.add(pinteLight);
- 
+  scene.add(light.target)
+  return light
 }
 
 
@@ -60,13 +85,14 @@ const addHelper = () => {
 }
 
 const createPlane = () => {
-  const planeGeometry = new THREE.PlaneGeometry(200, 200, 320, 300);
-  const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xeeeeee })
-  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane.receiveShadow = true;
-  plane.rotation.set(-Math.PI/2,0, 0);
-  scene.add(plane);
+  const geometry = new THREE.CircleGeometry(88, 50);
+  const material = new THREE.MeshStandardMaterial({ color: 0x666666 });
+  const circle = new THREE.Mesh(geometry, material);
+  circle.receiveShadow = true;
+  circle.rotation.set(-Math.PI / 2, 0, 0);
+  scene.add(circle);
 }
+
 
 
 // 加载模型
@@ -80,6 +106,8 @@ const loadModel = () => {
       CAR.value = obj;
       obj.traverse((item) => {
         if(item instanceof THREE.Mesh){
+          item.castShadow = true; //default is false
+          item.receiveShadow = false; //default
           switch (item.name) {
             case 'WHEEL_COLOR':
               WHEEL_COLOR.value = item
@@ -108,17 +136,27 @@ const loadModel = () => {
 
 
 const init = (container: HTMLElement) => {
-  container.appendChild(renderer.domElement);
-  addLight();
-  addHelper();
-  createPlane();
+  setting()
+  const light = addLight();
+  // addHelper();
   loadModel();
+  createPlane();
+  let angle = 0
+  container.appendChild(renderer.domElement);
   renderer.setAnimationLoop(() => {
+    angle += 0.005
+    if(angle>=360) {
+      angle = 0
+    }
+    light.position.setX(Math.sin(angle) * 50)
+    light.position.setZ(Math.cos(angle) * 50)
+    scene.rotateY(-0.001)
     controls.update()
     renderer.render(scene, camera)
   })
 }
 
+// TODO 修改轮毂和刹车颜色
 
 const changeColor = (HEX:string) => {
   if (BODY_COLOR){
@@ -127,37 +165,89 @@ const changeColor = (HEX:string) => {
 }
 
 
-const container = ref()
 onMounted(() => {
-  init(container.value);
+  if(container.value){
+    init(container.value);
+  }
 })
-
 
 </script>
 
 <style lang="scss" scoped>
 .UI {
-  display: flex;
   position: absolute;
   top: 0;
   left:  0;
   width: 100%;
   height: 100%;
   pointer-events: none;
-  .colors{
-    span{
-      pointer-events: all;
-      display: inline-block;
-      width: 20px;
-      height: 50px;
-      cursor: pointer;
-      margin-right: 20px;
-      transform: all 0.5s;
-      &:hover {
-        box-shadow: 5px 5px 20px rgba($color: #fdffe7, $alpha: 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  ul{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    list-style: none;
+    background-color: rgba($color: #000000, $alpha: 0);
+    padding: 30px 0 10px;
+    li {
+      button{
+        display: flex;
+        height: 50px;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        border: none;
+        background-color: rgba($color: #000, $alpha: 0);
+        pointer-events: all;
+        cursor: pointer;
+        text-shadow: 0 0 10px rgba($color: #000, $alpha: 0.2);
+        font-size: 10px;
+        font-weight: bolder;
+        color: #fff;
+        img {
+          width: 24px;
+          filter:invert(100%) drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.6));
+        }
+        img.carbody{
+          filter:invert(100%) drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.6));
+          width: 44px;
+        }
       }
     }
   }
+    .colors {
+      display: flex;
+      width: 300px;
+      justify-content: center;
+      padding-top: 20px;
+      filter: drop-shadow(0 0 5px rgba(255,255,255,0.2));
+      transition: all 0.5s;
+      pointer-events: all;
+      gap: 0px;
+      &:hover{
+        gap: 10px;
+      }
+      span {
+        pointer-events: all;
+        display: inline-block;
+        width: 20px;
+        height: 50px;
+        cursor: pointer;
+        transition: all 0.2s;
+        &:hover {
+          transform: translate(0, -5px);
+          box-shadow: 0 0 10px rgba($color: #ffffff, $alpha: 0.3);
+        }
+      }
+  
+      span.selected {
+        box-shadow: 0 0 10px rgba($color: #ffffff, $alpha: 0.3);
+      }
+    }
 }
 .car-diy{
   width: 100%;
