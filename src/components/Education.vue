104073881d2c1c327ca2ccb58ebacc74bc7d0a56
sudongpeng <sudongpeng@uino.com>
2023-02-10 15:28:46 +0800

    chore: 整理一下没有使用的组件

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -9,13 +9,12 @@ import school from '@/assets/images/school.jpg'
       <h1>教育经历</h1>
       <h2>毕业于广西科技大学</h2>
       <p>
-        大学坐落在螺蛳粉的故乡，它是一所本科院校。主修软件工程。
+        它是一所本科院校，坐落在螺蛳粉的故乡。
       </p>
       <p>
-        主要课程有Web开发，软件工程导论，面向对象程序设计。
+        主修软件工程, 相关课程有Web开发，软件工程导论，面向对象程序设计。
       </p>
     </div>
-    <!-- <img :src="school" alt=""> -->
   </section>
 </template>
 
